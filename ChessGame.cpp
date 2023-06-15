38a41717361b317ec35a609ae2c808ec2be03d15
spotts01 <sean-potts@outlook.com>
2023-06-15 15:13:15 -0400

    working on chessboard class. Changed chess board to hold peices

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -7,19 +7,20 @@
 using namespace std;
 
 
-enum Pieces{pawn, knight, bishop, castle, queen, king};
+
+enum Pieces {pawn, knight, bishop, castle, queen, king};
+enum Colors {white, black};
+
 class ChessPeice
 {
 public:
 	Pieces curPiece;
+	Colors color;
 
-	ChessPeice(Pieces piece)
-
+	ChessPeice(Pieces piece, Colors color)
 	{
-		curPiece = piece;
-		//this->color = color;
-		//this->canMove = move;
-
+		this->curPiece = piece;
+		this->color = color;
 	}
 
 	ChessPeice()
@@ -30,18 +31,12 @@ public:
 
 	}
 	
-	string getColor() {
-		return color;
-	}
-
-	void setColor(string setColor) {
-		color = setColor;
-	}
+	
+	// how it should move
 
 private:
 
-	string color; 
-	bool canMove;
+	//bool canMove;
 
 };
 
@@ -53,28 +48,32 @@ class ChessBoard
 public:
 
 	// store refereences to objects as pointers
-	ChessPeice* board[8][8]; 
+	// if chessBoard is NULL there is an empty space to move to. 
+	ChessPeice* board[8][8] = { NULL };
 
+	//tell peice if it can move
 
 	ChessBoard()
 	{
+		Colors colorOfPiece = white;
 		// sets up chess board
-		board[0][0] = new ChessPeice(castle);
+	
 		for (int j = 0; j < 8; j+=7) {
-			board[j][0] = new ChessPeice(castle);
-			board[j][1] = new ChessPeice(knight);
-			board[j][2] = new ChessPeice(bishop);
-			board[j][4] = new ChessPeice(queen);
-			board[j][3] = new ChessPeice(king);
-			board[j][5] = new ChessPeice(bishop);
-			board[j][6] = new ChessPeice(knight);
-			board[j][7] = new ChessPeice(castle);
+			board[j][0] = new ChessPeice(castle, colorOfPiece);
+			board[j][1] = new ChessPeice(knight, colorOfPiece);
+			board[j][2] = new ChessPeice(bishop, colorOfPiece);
+			board[j][4] = new ChessPeice(queen, colorOfPiece);
+			board[j][3] = new ChessPeice(king, colorOfPiece);
+			board[j][5] = new ChessPeice(bishop, colorOfPiece);
+			board[j][6] = new ChessPeice(knight, colorOfPiece);
+			board[j][7] = new ChessPeice(castle, colorOfPiece);
+			colorOfPiece = black;
 		}
 		for (int i = 0; i < 8; i++) {
-			board[1][i] = new ChessPeice(pawn);
+			board[1][i] = new ChessPeice(pawn, white);
 		}
 		for (int i = 0; i < 8; i++) {
-			board[6][i] = new ChessPeice(pawn);
+			board[6][i] = new ChessPeice(pawn, black);
 		}
 
 	}
@@ -87,20 +86,49 @@ private:
 
 };
 
+class Player
+{
+
+public:
+
+	string stateOfGame;
+	string name;
+	Colors playerColor;
+	int numberTurns = 0;
+
+	Player()
+	{
+	}
+	Player(string name, Colors color)
+	{
+		this->name = name;
+		this->playerColor = color;
+
+	}
+	~Player()
+	{
+	}
+
+private:
+
+};
 
 
 class Game
 {
 public:
 
-	//Player playersInGame;
-	//essBoard chessGame;
 	
 
-	Game()
-	{
 
+	Game(string player1, string player2, Colors player1Color, Colors player2Color) :player1(player1, player1Color), player2(player2, player2Color), chessGame()
+	{
+		cout << "New Game Created!" << endl;
 	}
+
+	Player player1;
+	Player player2;
+	ChessBoard chessGame;
 
 	~Game()
 	{
@@ -113,26 +141,6 @@ private:
 
 
 
-class Player
-{
-	
-public:
-
-	string stateOfGame;
-	string name; 
-	int numberTurns;
-
-	Player()
-	{
-	}
-
-	~Player()
-	{
-	}
-
-private:
-
-};
 
 
 
@@ -141,6 +149,9 @@ private:
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ChessBoard chessboard;
+
+
+	
 }
 
