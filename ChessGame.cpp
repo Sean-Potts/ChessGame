87f568102e17151c4381df57b27f608ad6d44e86
spotts01 <sean-potts@outlook.com>
2023-06-15 19:17:42 -0400

    added a visual for the chessboard to display the peices with letters. Added a method to move the peices. So far no logic has been implmeneted for each peice. but i have tester functions to move peices arround to test out logic.

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -3,13 +3,31 @@
 #include <vector>
 #include <iostream>
 #include <string.h>
+#include <map>
 
 using namespace std;
 
 
 
-enum Pieces {pawn, knight, bishop, castle, queen, king};
-enum Colors {white, black};
+enum Pieces {pawn = 3, knight, bishop, castle, queen, king};
+enum Colors {white = 1, black};
+
+
+
+
+std::map<Colors, std::string> colorNames = {
+	{white, "W-"},
+	{black, "B-"}
+};
+
+std::map<Pieces, std::string> pieceNames = {
+	{pawn, "P"},
+	{knight, "K"},
+	{castle, "C"},
+	{queen, "Q"},
+	{bishop, "B"},
+	{king, "K"}
+};
 
 class ChessPeice
 {
@@ -78,9 +96,77 @@ public:
 
 	}
 
+	void swapElements(int row1, int col1, int row2, int col2) {
+
+		std::swap(this->board[row1][col1], this->board[row2][col2]);
+
+	}
+
 	~ChessBoard()
 	{
 	}
+	void MovePiece(Colors pieceColor) 
+	{
+		cout << "What piece do you want to move?" << endl;
+
+		// Prints ChessBoard for Playing/Testing
+		for (int i = 0; i < 8; ++i) {
+			for (int j = 0; j < 8; ++j) {
+				if (board[i][j] == NULL) {
+					cout << "EMP ";
+				}
+				else {
+					cout << colorNames[board[i][j]->color] << pieceNames[board[i][j]->curPiece] << " ";
+				}
+				
+			}
+			cout << i;
+			cout << std::endl;
+		}
+		for (int i = 0; i < 8; i++) {
+			cout <<  i << "   ";
+		}
+
+		cout << endl;
+		cout << "Select a piece using Column/Row formate" << endl;
+	
+		string piece;
+		cin >> piece;
+
+		cout << "Select a spot to move using Column/Row formate" << endl;
+		string location;
+		cin >> location;
+
+		cout << piece << location; 
+
+		int rowPiece = piece[0] - '0';
+		int colPiece = piece[1] - '0';
+		int rowMove = location[0] - '0';
+		int colMove = location[1] - '0';
+
+	
+		swapElements(rowPiece, colPiece, rowMove, colMove);
+		// Prints ChessBoard for Playing/Testing
+		for (int i = 0; i < 8; ++i) {
+			for (int j = 0; j < 8; ++j) {
+				if (board[i][j] == NULL) {
+					cout << "EMP ";
+				}
+				else {
+					cout << colorNames[board[i][j]->color] << pieceNames[board[i][j]->curPiece] << " ";
+				}
+
+			}
+			cout << i;
+			cout << std::endl;
+		}
+		for (int i = 0; i < 8; i++) {
+			cout << i << "   ";
+		}
+
+	}
+
+	
 
 private:
 
@@ -123,15 +209,36 @@ public:
 
 	Game(string player1, string player2, Colors player1Color, Colors player2Color) :player1(player1, player1Color), player2(player2, player2Color), chessGame()
 	{
+		if (player1Color == white) {
+			whosTurn = 1;
+		}
+		else {
+			whosTurn = 2;	
+		}
 		cout << "New Game Created!" << endl;
 	}
 
 	Player player1;
 	Player player2;
 	ChessBoard chessGame;
+	string stateOfGame;
+	int whosTurn; // 1 or 2;
 
 	~Game()
 	{
+	}
+
+	void MakeTurn() 
+	{
+		if (whosTurn == 1) 
+		{
+			chessGame.MovePiece(player1.playerColor);
+		}
+		else
+		{
+			chessGame.MovePiece(player2.playerColor);
+		}
+		
 	}
 
 private:
@@ -150,6 +257,8 @@ private:
 int main()
 {
 	ChessBoard chessboard;
+
+	chessboard.MovePiece(white);
 
 
 	
